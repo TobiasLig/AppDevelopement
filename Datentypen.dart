c685cc1ae05c8f7cd5d6cb276e40dd67a048df14
TobiasLig <68202280+TobiasLig@users.noreply.github.com>
2020-07-13 14:44:24 +0200

    Create Datentypen.dart

---
+++ Datentypen.dart
@@ -0,0 +1,21 @@
+main() {
+  
+  int zahl = 1;
+  int zahl2 = 2;
+  
+  print(zahl);
+  print(zahl2);
+  
+  double zahl3 = 1.1;
+  double zahl4 = 2.2;
+  
+  print(zahl3);
+  print(zahl4);
+  
+  String buchstabe = "a";
+  String buchstabe2 = "b";
+  
+  print(buchstabe);
+  print(buchstabe2);
+
+}
