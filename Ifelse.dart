f5a2c3a38c454ba2556cee549d27342ecff10e8c
TobiasLig <68202280+TobiasLig@users.noreply.github.com>
2020-07-14 11:16:24 +0200

    Create Ifelse.dart

---
+++ Ifelse.dart
@@ -0,0 +1,13 @@
+main() {
+  
+  int zahlengrenze = 11;
+  if (zahlengrenze > 10){
+    print("Größer als 10");
+  }else{
+    print("Kleiner als 10");
+  }
+  String zahlengrenze2 = zahlengrenze > 10 ? "Größer als 10" : "Kleiner als 10";
+  print(zahlengrenze2);
+  
+
+}
