965223ffb317001a2f1151f67f0fcc301bc39068
TobiasLig <68202280+TobiasLig@users.noreply.github.com>
2020-07-14 10:33:47 +0200

    Create Stringverarbeitung.dart

---
+++ Stringverarbeitung.dart
@@ -0,0 +1,11 @@
+main() {
+  
+  String meinName = "Tobias";
+    print("Hallo mein Name ist $meinName");
+    double zahl = 2.1;
+    String zahlenwert = zahl.toString();
+  print("Die Zahl ist $zahlenwert");
+    int lang = meinName.length;
+  print("Der Name ist $lang Buchstaben lang");
+
+}
